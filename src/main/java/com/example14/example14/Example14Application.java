6fa0ebf160976ac54f1d5f03f1ad6dea02d37c11
exampleOne <a.vasilievaaa@yandex.ru>
2022-03-29 20:22:48 +0300

    первый коммит

--- src/main/java/com/example14/example14/Example14Application.java
+++ src/main/java/com/example14/example14/Example14Application.java
@@ -1,13 +1,7 @@
 package com.example14.example14;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class Example14Application {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Example14Application.class, args);
-    }
-
+public static void main(String[] args) { SpringApplication.run(Example14Application.class, args);}
 }
