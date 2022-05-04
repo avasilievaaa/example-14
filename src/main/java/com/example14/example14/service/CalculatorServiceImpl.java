842b3b7cdcff0df924d38bbc3ca9493a89af77c3
exampleOne <a.vasilievaaa@yandex.ru>
2022-05-04 22:14:52 +0300

    Домашнее задание №2.12

--- src/main/java/com/example14/example14/service/CalculatorServiceImpl.java
+++ src/main/java/com/example14/example14/service/CalculatorServiceImpl.java
@@ -1,14 +1,29 @@
 package com.example14.example14.service;
 
 import org.springframework.stereotype.Service;
+
 @Service
 public class CalculatorServiceImpl implements CalculatorService {
-@Override
-public int sum (int num1, int num2) {return num1 + num2;}
-@Override
-public int subtraction(int num1, int num2) {return num1 - num2;}
-@Override
-public int multiplication(int num1, int num2) {return num1 * num2;}
-@Override
-public int division(int num1, int num2) {return num1 / num2;}
+    @Override
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public int subtraction(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    @Override
+    public int multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public int division(int num1, int num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("You cannot divide by zero!");
+        }
+        return num1 / num2;
+    }
 }
